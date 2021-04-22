5260b686f413614be61b38625f5b297089a30a34
jrdrenth <john.drenth@gmail.com>
2021-04-22 15:44:38 -0700

    Updated app & seeder to use Atlas MongoDB instead of local

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,11 @@
-let mongoose = require("mongoose");
-let db = require("../models");
+let mongoose = require('mongoose');
+let db = require('../models');
 
-mongoose.connect("mongodb://localhost/workout", {
+require("dotenv").config();
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+//mongoose.connect('mongodb://localhost/workoutdb', {
+mongoose.connect(MONGODB_URI, {  
   useNewUrlParser: true,
   useFindAndModify: false
 });
@@ -11,12 +15,18 @@ let workoutSeed = [
     day: new Date(new Date().setDate(new Date().getDate()-10)),
     exercises: [
       {
-        type: "resistance",
-        name: "Bicep Curl",
+        type: 'resistance',
+        name: 'Bicep Curl',
         duration: 20,
         weight: 100,
         reps: 10,
         sets: 4
+      },
+      {
+        type: 'cardio',
+        name: 'Bike Riding',
+        duration: 60,
+        distance: 12
       }
     ]
   },
@@ -24,8 +34,8 @@ let workoutSeed = [
     day: new Date(new Date().setDate(new Date().getDate()-9)),
     exercises: [
       {
-        type: "resistance",
-        name: "Lateral Pull",
+        type: 'resistance',
+        name: 'Lateral Pull',
         duration: 20,
         weight: 300,
         reps: 10,
@@ -37,8 +47,8 @@ let workoutSeed = [
     day: new Date(new Date().setDate(new Date().getDate()-8)),
     exercises: [
       {
-        type: "resistance",
-        name: "Push Press",
+        type: 'resistance',
+        name: 'Push Press',
         duration: 25,
         weight: 185,
         reps: 8,
@@ -50,8 +60,8 @@ let workoutSeed = [
     day: new Date(new Date().setDate(new Date().getDate()-7)),
     exercises: [
       {
-        type: "cardio",
-        name: "Running",
+        type: 'cardio',
+        name: 'Running',
         duration: 25,
         distance: 4
       }
@@ -61,8 +71,8 @@ let workoutSeed = [
     day: new Date(new Date().setDate(new Date().getDate()-6)),
     exercises: [
       {
-        type: "resistance",
-        name: "Bench Press",
+        type: 'resistance',
+        name: 'Bench Press',
         duration: 20,
         weight: 285,
         reps: 10,
@@ -74,8 +84,8 @@ let workoutSeed = [
     day: new Date(new Date().setDate(new Date().getDate()-5)),
     exercises: [
       {
-        type: "resistance",
-        name: "Bench Press",
+        type: 'resistance',
+        name: 'Bench Press',
         duration: 20,
         weight: 300,
         reps: 10,
@@ -87,8 +97,8 @@ let workoutSeed = [
     day: new Date(new Date().setDate(new Date().getDate() - 4)),
     exercises: [
       {
-        type: "resistance",
-        name: "Quad Press",
+        type: 'resistance',
+        name: 'Quad Press',
         duration: 30,
         weight: 300,
         reps: 10,
@@ -100,16 +110,16 @@ let workoutSeed = [
     day: new Date(new Date().setDate(new Date().getDate() - 3)),
     exercises: [
       {
-        type: "resistance",
-        name: "Bench Press",
+        type: 'resistance',
+        name: 'Bench Press',
         duration: 20,
         weight: 300,
         reps: 10,
         sets: 4
       },
       {
-        type: "cardio",
-        name: "Bike Riding",
+        type: 'cardio',
+        name: 'Bike Riding',
         duration: 60,
         distance: 12
       }
@@ -119,12 +129,18 @@ let workoutSeed = [
     day: new Date(new Date().setDate(new Date().getDate() - 2)),
     exercises: [
       {
-        type: "resistance",
-        name: "Military Press",
+        type: 'resistance',
+        name: 'Military Press',
         duration: 20,
         weight: 300,
         reps: 10,
         sets: 4
+      },
+      {
+        type: 'cardio',
+        name: 'Bike Riding',
+        duration: 60,
+        distance: 12
       }
     ]
   }
@@ -133,7 +149,7 @@ let workoutSeed = [
 db.Workout.deleteMany({})
   .then(() => db.Workout.collection.insertMany(workoutSeed))
   .then(data => {
-    console.log(data.result.n + " records inserted!");
+    console.log(data.result.n + ' records inserted!');
     process.exit(0);
   })
   .catch(err => {
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require("dotenv").config();
 
-const PORT = process.env.PORT || 3001;
-
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const PORT = process.env.APP_PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -10,7 +11,7 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+mongoose.connect(MONGODB_URI || 'mongodb://localhost/workoutdb', {
   useNewUrlParser: true,
   useFindAndModify: false
 });
